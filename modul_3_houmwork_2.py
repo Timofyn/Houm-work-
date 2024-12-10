d95d485caceab3ce65f6c5c3ce63aa15c8baa69b
Timofyn <dolzhi@mail.ru>
2024-12-10 15:07:20 +0300

    Update modul_3_houmwork_2.py

--- modul_3_houmwork_2.py
+++ modul_3_houmwork_2.py
@@ -1,6 +1,6 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
     okonchania = ('.com', '.ru', '.net')
-    if sender and recipient in '@' or not sender.endswith(okonchania) or \
+    if sender or recipient in '@' or not sender.endswith(okonchania) or \
              not recipient.endswith(okonchania):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif sender == recipient:
@@ -15,3 +15,5 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Напоминаю самому себе о вебинаре', 'teachermail.ru', sender='teachermail.ru')
+send_email('Напоминаю самому себе о вебинаре', 'teachermail.ru', sender='university.help@gmail.com')
